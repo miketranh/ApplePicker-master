277f930221707373e8beaaab0a55111286c7bdca
miketranh <miketranh21@gmail.com>
2016-06-07 00:26:44 -0400

    setting up for success
    made folder and component

---
+++ Assets/C#/AppleTree.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AppleTree : MonoBehaviour {
+
+	public GameObject ApplePrefab;
+	public float speed = 1f;
+	public float leftAndRightEdge = 10f;
+	public float chanceToChangeDirections = 0.1f;
+	public float secondsBetweenAppleDrops = 1f;
+
+
+	void Start () {
+		InvokeRepeating ("DropApple", 2f, secondsBetweenAppleDrops);
+	
+	}
+	void DropApple(){
+		GameObject Apple = Instantiate (ApplePrefab) as GameObject;
+		Apple.transform.position = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//movement
+		Vector3 pos = transform.position;
+		pos.x += speed * Time.deltaTime;
+		transform.position = pos;
+		//change direction
+		if (pos.x < -leftAndRightEdge) {
+			speed = Mathf.Abs
+				(speed);
+			//move right
+		} else
+			if (pos.x > leftAndRightEdge) {
+			speed = -Mathf.Abs
+				(speed);
+			//move left
+		}
+	}
+	void FixedUpdate(){
+			if (Random.value < chanceToChangeDirections) {
+				speed *= -1;
+			}
+	}
+}
